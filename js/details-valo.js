b97a9e9a25ec4686c7f0ae8448474dbaf7332d57
cgodumnnabuike <ebubeodum@gmail.com>
2023-01-11 21:51:58 +0000

    Complete formating of project

--- js/details-valo.js
+++ js/details-valo.js
@@ -1,55 +1,42 @@
-function loadData(url,callback)
-{
-	fetch(url).then(function(response) {
-		return response.json();
-	}).then(function(json) {
-		callback(json)
-	});
+function loadData(url, callback) {
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      callback(json);
+    });
 } //end of loadData()
 
+function populateContent(agent) {
+  console.log(agent.data);
+  const displayNameEl = document.querySelector("#displayName");
+  const descriptionEl = document.querySelector("#description");
+  const abilitiesEl = document.querySelector("#abilities");
+  const roleEl = document.querySelector("#role");
+  displayNameEl.textContent = agent.data.displayName;
+  descriptionEl.textContent = agent.data.description;
+  const carouselEl = document.getElementById("carousel-image");
+  const carouselEl2 = document.getElementById("carousel-image1");
+  carouselEl.src = agent.data.fullPortrait;
+  carouselEl2.src = agent.data.background;
+  roleEl.textContent = `${agent.data.role.displayName} - ${agent.data.role.description}`;
 
-
-
-
-function populateContent(agent)
-{
-	
-	console.log(agent.data);
-	const displayNameEl=document.querySelector("#displayName");
-	const descriptionEl=document.querySelector("#description");
-	const abilitiesEl=document.querySelector("#abilities");
-	const roleEl=document.querySelector("#role");
-	displayNameEl.textContent = agent.data.displayName;
-	descriptionEl.textContent = agent.data.description;
-	const carouselEl=document.getElementById('carousel-image');
-	const carouselEl2=document.getElementById('carousel-image1');
-	carouselEl.src= agent.data.fullPortrait;
-	carouselEl2.src = agent.data.background;
-    roleEl.textContent = `${agent.data.role.displayName} - ${agent.data.role.description}`;
-	
-	
-	const abilities = agent.data.abilities
-	abilities.forEach(function(ability){
-		const abiLi=document.createElement("li");
-		console.log(ability.description)
-		abiLi.textContent=`${ability.displayName} - ${ability.description}`;
-		abilitiesEl.appendChild(abiLi);
-	});
-
-	
-
-
+  const abilities = agent.data.abilities;
+  abilities.forEach(function (ability) {
+    const abiLi = document.createElement("li");
+    console.log(ability.description);
+    abiLi.textContent = `${ability.displayName} - ${ability.description}`;
+    abilitiesEl.appendChild(abiLi);
+  });
 }
 
-
-function init(){
-	//URLSearchParams provides an easy method for getting data from the querystring e.g. details.html?id=3
-	//see https://davidwalsh.name/query-string-javascript for more info
-	const urlParams = new URLSearchParams(window.location.search);
-	const id = urlParams.get("id");
-	loadData("https://valorant-api.com/v1/agents/"+id ,populateContent);
-	
+function init() {
+  //URLSearchParams provides an easy method for getting data from the querystring e.g. details.html?id=3
+  //see https://davidwalsh.name/query-string-javascript for more info
+  const urlParams = new URLSearchParams(window.location.search);
+  const id = urlParams.get("id");
+  loadData("https://valorant-api.com/v1/agents/" + id, populateContent);
 }
-
 
 init();
--- js/valorant.js
+++ js/valorant.js
@@ -1,37 +1,37 @@
-function loadData(url,callback)
-{
-	fetch(url).then(function(response) {
-		return response.json();
-	}).then(function(json) {
-		callback(json)
-	});
+function loadData(url, callback) {
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      callback(json);
+    });
 } //end of loadData()
 
-function populateList(agents)
-{
-	console.log(agents.data);
-	const agentsFragment = document.createDocumentFragment();
-	agents.data.forEach(function(agent){
-		const newLi=document.createElement("li");
-		const newLink=document.createElement("a");
-		const newImg=document.createElement("img");
-		newLink.textContent=agent.displayName;
-		newImg.src=agent.killfeedPortrait;
-		//adds a querystring to the URL e.g. details.html?id=2
-		newLink.setAttribute("href","details.html?id="+agent.uuid);
-		newLi.appendChild(newLink);
-		newLi.appendChild(newImg);
-		agentsFragment.appendChild(newLi);
-	})
-	const agentsList=document.querySelector("#Agent-list");
-	agentsList.appendChild(agentsFragment);
-	
+function populateList(agents) {
+  console.log(agents.data);
+  const agentsFragment = document.createDocumentFragment();
+  agents.data.forEach(function (agent) {
+    const newLi = document.createElement("li");
+    const newLink = document.createElement("a");
+    const newImg = document.createElement("img");
+    newLink.textContent = agent.displayName;
+    newImg.src = agent.killfeedPortrait;
+    //adds a querystring to the URL e.g. details.html?id=2
+    newLink.setAttribute("href", "details.html?id=" + agent.uuid);
+    newLi.appendChild(newLink);
+    newLi.appendChild(newImg);
+    agentsFragment.appendChild(newLi);
+  });
+  const agentsList = document.querySelector("#Agent-list");
+  agentsList.appendChild(agentsFragment);
 } //end of populateList, do not remove this line
 
-function init(){
-	
-
-	loadData ("https://valorant-api.com/v1/agents?isPlayableCharacter=true",populateList);
+function init() {
+  loadData(
+    "https://valorant-api.com/v1/agents?isPlayableCharacter=true",
+    populateList
+  );
 }
 
 init();
